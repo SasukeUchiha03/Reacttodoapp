79c1c7b877a9594f9e84f6337964aa37bee94727
SasukeUchiha03 <faruk.yeni@aol.com>
2026-06-24 12:31:00 +0200

    Add bucket list UI with state management and styling.

--- bucket-list/src/App.jsx
+++ bucket-list/src/App.jsx
@@ -2,8 +2,39 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [bucketList, setBucketList] = useState([
+    "Fallschirmspringen",
+    "Eine Weltreise machen",
+    "Ein Buch schreiben",
+  ]);
 
-    return <h1>Hallo!</h1>;
+  const [input, setInput] = useState("")
+
+  function addItem() {
+    if(input === "") return;
+
+    setBucketList([...bucketList, input])
+    setInput("");
+
+  }
+
+
+  return (
+    <>
+      <h1>Meine Bucket List</h1>
+      <input
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      placeholder="Mein nächstes Ziel..."
+      />
+      <button onClick={addItem}>Hinzufügen</button>
+      <div className="list">
+        {bucketList.map((item, index) => (
+          <div className="list-item" key={index}>{item}</div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App
